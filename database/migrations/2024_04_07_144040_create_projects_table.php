07f92042e755b5f8ac6e5fb97e7c107bea80ee08
isumalatov <isumalatov@gmail.com>
2024-04-07 16:54:47 +0200

    migrations created

---
+++ database/migrations/2024_04_07_144040_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('publication_date')->nullable();
+            $table->timestamp('completion_date')->nullable();
+            $table->decimal('required_funds', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
