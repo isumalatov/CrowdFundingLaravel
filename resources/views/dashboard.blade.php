79617966f66c8af8137d0ab431d13a4f098c0c67
isumalatov <isumalatov@gmail.com>
2024-04-12 01:04:47 +0200

    dashboard added

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!-- resources/views/dashboard.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="/">Home</a> |
+            <a href="/dashboard">Dashboard</a> |
+            <!-- Agrega más enlaces según sea necesario -->
+        </nav>
+    </header>
+    <h1>Dashboard</h1>
+</body>
+</html>
