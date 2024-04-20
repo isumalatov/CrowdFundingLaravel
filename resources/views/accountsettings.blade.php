615017b5f9e238b938481a25969b971c2588e6eb
Alejandro Garcia <aggg12@alu.ua.es>
2024-04-20 13:55:29 +0200

    77042008N a punto de terminar lo mio (un problema del server)

---
+++ resources/views/accountsettings.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Settings</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/accountsettings.css') }}"> 
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="/">Home</a> |
+            <a href="/dashboard">Dashboard</a> |
+            <a href="/accountsettings">Account Settings</a> 
+        </nav>
+    </header>
+    <div class="container">
+        <div class="sidebar">
+            
+        </div>
+        <div class="main-content">
+            <h1>Account Settings</h1>
+            
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountSettingsController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/accountsettings', [AccountSettingsController::class, 'index'])->name('accountsettings.index');
+
